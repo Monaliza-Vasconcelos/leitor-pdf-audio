edfbb338ded384f49aad41ef4d52ef76176420c8
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2026-04-29 10:08:26 -0300

    Enviando arquivos de configuração render e procfile

--- leitorpdf/settings.py
+++ leitorpdf/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)h6dzbew1p4$iwm_klisbas2_rzeg)5eu+a)9b+ebhvey65e)l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
